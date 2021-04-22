cc6912c30b36fc95477d4bbee09cfaa37f10c3b0
schau-sjsu <78131171+schau-sjsu@users.noreply.github.com>
2021-04-21 21:44:52 -0700

    Add form for account home page
    Add fields for logout and marking task as complete

--- app/forms.py
+++ app/forms.py
@@ -2,9 +2,23 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, IntegerField
 from wtforms.validators import DataRequired
 
-class Form(FlaskForm):
+class LoginForm(FlaskForm):
     """
     
     """
     
+
+class OverviewForm(FlaskForm):
+    """
+    Represent the form for the account home page.
+
+    Attributes
+    ----------
+    logout : SubmitField
+        Button for user to log out.
+    complete : BooleanField
+        Checkbox for user to mark task as complete.
+    """
+    logout = SubmitField('Log out')
+    complete = BooleanField('Mark as complete')
 
