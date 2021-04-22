db753727019466578e18167badf33fdca1185ba8
schau-sjsu <78131171+schau-sjsu@users.noreply.github.com>
2021-04-21 21:46:36 -0700

    Add Task table
    Create minimum attributes needed for marking task as complete

--- app/models.py
+++ app/models.py
@@ -12,7 +12,30 @@ class User(db.Model):
     def __repr__(self):
         return f'<User {self.username}>'
 
+class Task(db.Model):
+    """
+    Set up table with task information.
 
+    Attributes
+    ----------
+    id : IntegerField
+        Id of task.
+    title : StringField
+        Title of task.
+    user_id : IntegerField
+        Id of user who created the task.
+    """
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False, unique=True)
+    # know which account the task belongs to
+    user_id = dbColumn(db.Integer, db.ForeignKey(user.id))
+
+    """
+    Output name of task.
+    """
+    def __repr__(self):
+        return f'<Task {self.title}>'
 
 # Need to have tables created in order to access this form
 db.create_all()
+
