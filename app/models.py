a65c9852eb7822a5aa7c035fc461399bbbee69ca
kdominguez49 <78886694+kdominguez49@users.noreply.github.com>
2021-05-05 18:16:04 -0700

    Update models.py
    remove line

--- app/models.py
+++ app/models.py
@@ -8,8 +8,6 @@ from flask_login import UserMixin
 shares = db.Table('shares',
                   db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
                   db.Column('task_id', db.Integer, db.ForeignKey('task.id')),
-                  #testing to fix error
-                  db.Column('task_priority', db.Integer, db.ForeignKey('task.priority'))
                   )
 
 class User(UserMixin, db.Model):
@@ -62,7 +60,6 @@ class Task(UserMixin, db.Model):
     description = db.Column(db.String, nullable=True, unique=False)
     # know which account the task belongs to
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
-
     # Setting up one column for deadline
     deadline = db.Column(db.String, nullable=True, unique=False)
     # Create column for priority
@@ -80,12 +77,12 @@ class Task(UserMixin, db.Model):
             Date that the task needs to be done by.
         """
         self.deadline = date
-
     """
     Output name of task.
     """
     def __repr__(self):
         return f'<Task {self.title}>'
+      
       
     def setPriority(self, priority):
         """
@@ -99,7 +96,6 @@ class Task(UserMixin, db.Model):
             priority of the task.
         """
         self.priority = priority
-
     """
     Output name of task.
     """
